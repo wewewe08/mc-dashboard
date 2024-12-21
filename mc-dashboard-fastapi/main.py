f8a5b0b020ffc11ab26162b0757e8f9aa4ebd092
yvonne <yvonnehuang0810@gmail.com>
2024-12-21 01:07:34 -0500

    removed old skin api

--- mc-dashboard-fastapi/main.py
+++ mc-dashboard-fastapi/main.py
@@ -4,6 +4,7 @@ import requests
 import base64
 import json
 from model import Player
+from datetime import datetime
 
 app = FastAPI()
 
@@ -26,14 +27,19 @@ def read_root():
 async def get_user_names(uuid: str):
     uuid_clean = uuid.replace("-", "")
     user_response = requests.get(f"https://sessionserver.mojang.com/session/minecraft/profile/{uuid_clean}").json()
-    skin_response = requests.get(f"https://mc-heads.net/avatar/{uuid}")
     base64_decoded = base64.b64decode(user_response['properties'][0]['value'])
     try:
         json_data = json.loads(base64_decoded.decode('utf-8'))
+
+        timestamp_seconds = (json_data.get('timestamp', 'Unknown')) / 1000
+        formatted_timestamp = datetime.utcfromtimestamp(timestamp_seconds).strftime('%Y-%m-%d %H:%M:%S')
+
         username = json_data.get('profileName', 'Unknown')
+        skin = json_data.get('textures', {}).get('SKIN', {}).get('url', 'Unknown')
         return {
+            'timestamp': formatted_timestamp,
             'username': username,
-            'avatar': skin_response
+            'skin': skin
         }
     except json.JSONDecodeError as e:
         print(f"Error decoding JSON: {e}")
